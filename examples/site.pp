3803d6e0f0ef485ed3de76d05255f0659cbe9ebc
Michael Chapman <woppin@gmail.com>
2014-03-03 15:45:58 +1100

    Fix linting issues

--- examples/site.pp
+++ examples/site.pp
@@ -1,8 +1,7 @@
 node default {
-    class { 'galera':
-       galera_servers => ['192.168.99.101','192.168.99.102'],
-       galera_master  => 'control1.domain.name',
-       vendor_type => 'mariadb'
-    }
-
+  class { 'galera':
+    galera_servers => ['192.168.99.101','192.168.99.102'],
+    galera_master  => 'control1.domain.name',
+    vendor_type    => 'mariadb'
+  }
 }
