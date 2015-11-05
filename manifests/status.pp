6508cf5100ffc1bfb92f81e1c596ef784580cb86
Risto Laurikainen <risto.laurikainen@csc.fi>
2015-11-05 13:08:07 +0200

    Set max cps value for mysqlchk to 300
    The default value of 50 connections per second can cause issues when a
    large number of nodes are using HAProxy to load balance Galera. Set a
    slightly larger value of 300.

--- manifests/status.pp
+++ manifests/status.pp
@@ -74,6 +74,7 @@ class galera::status (
     service_type            => 'UNLISTED',
     user                    => 'nobody',
     flags                   => 'REUSE',
+    cps                     => '300 10',
     log_on_success          => '',
     log_on_success_operator => '=',
     before                  => Anchor['mysql::server::end'],
