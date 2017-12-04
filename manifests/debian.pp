2bfb2b70471916be000a0a984a8880c43854ccaf
Lucas Magno Lima Bracarense <magno.lucas@dcc.ufmg.br>
2017-12-04 17:01:09 -0200

    Fix bootstrap in mysql-5.7
    Mysql-wsrep-5.7 package was leaving a file (debian-5.7.flag) in
    mysql datadir, what caused an error in the database installation.
    
    This commit ensures that the file is not there before installing
    the databases.

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -45,6 +45,12 @@ class galera::debian {
     require     => Class['mysql::server::install'],
   }
 
+  file { '/var/lib/mysql/debian-5.7.flag':
+    ensure  => absent,
+    before  => Class['mysql::server::installdb'],
+    require => Class['mysql::server::install'],
+  }
+
   # Assign this locally so that it is in scope for the template below.
   # Required for Puppet 4
   $deb_sysmaint_password = $galera::deb_sysmaint_password
