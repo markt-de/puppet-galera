9bb94043316fb84f819e1907034911bc52ec05f4
Frank Wall <fw@moov.de>
2018-09-03 21:39:54 +0200

    cleanup, refs #110

--- examples/centos7.pp
+++ examples/centos7.pp
@@ -1,9 +1,10 @@
 node default {
   class { 'galera':
-    galera_servers  => ['192.168.99.101','192.168.99.102'],
-    galera_master   => 'control1.domain.name',
+    galera_servers  => ['10.0.99.101', '10.0.99.102'],
+    galera_master   => 'node1.example.com',
     vendor_type     => 'mariadb',
-    status_password => 'mariadb',
+    root_password   => 'pa$$w0rd',
+    status_password => 'pa$$w0rd',
     bind_address    => $::ipaddress_enp0s8,
   }
 }
