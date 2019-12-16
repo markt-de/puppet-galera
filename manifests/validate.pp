52c67fb663e29de018e7b740dad4ae06bc3b0e0a
efoft <dubskiy.alexander@gmail.com>
2019-12-16 18:21:03 +0300

    galera::status must be conditionally included

--- manifests/validate.pp
+++ manifests/validate.pp
@@ -7,17 +7,35 @@
 # This is used because after returning success,
 # the service is still not quite ready.
 #
+# We can validate connection either with
+#   1) root password if given or
+#   2) status password if status_check is true
+# 
 class galera::validate(
   String $action,
   String $catch,
   Integer $delay,
-  String $host = $galera::status_host,
-  String $password = $galera::status_password,
   Integer $retries,
-  String $user = $galera::status_user,
   Optional[String] $inv_catch,
 ) {
-  include galera::status
+
+  if $galera::root_password =~ String {
+    $validate_host     = 'localhost'
+    $validate_user     = 'root'
+    $validate_password = $galera::root_password
+    $validate_require  = Class['mysql::server::root_password']
+  }
+  elsif $galera::status_check {
+    include galera::status
+
+    $validate_host     = $galera::status_host
+    $validate_user     = $galera::status_user
+    $validate_password = $galera::status_password
+    $validate_require  = Class['galera::status']
+  }
+  else {
+    fail('Cannot validate connection without root_password or status_check')
+  }
 
   if $catch {
     $truecatch = $catch
@@ -27,7 +45,7 @@ class galera::validate(
     fail('No catch method specified in galera validation script')
   }
 
-  $cmd = "mysql --host=${host} --user=${user} --password=${password} -e '${action}'"
+  $cmd = "mysql --host=${validate_host} --user=${validate_user} --password=${validate_password} -e '${action}'"
   exec { 'validate_connection':
     path        => '/usr/bin:/bin:/usr/sbin:/sbin',
     provider    => shell,
@@ -37,7 +55,7 @@ class galera::validate(
     subscribe   => Service['mysqld'],
     refreshonly => true,
     before      => Anchor['mysql::server::end'],
-    require     => Class['galera::status']
+    require     => $validate_require,
   }
 
   Exec<| title == 'bootstrap_galera_cluster' |> ~> Exec['validate_connection']
