b48ba7dab09b6775c0283320e4c58a98899f26c4
Frank Wall <fw@moov.de>
2020-02-10 23:11:00 +0100

    simplify galera::status by removing useless local variables

--- manifests/status.pp
+++ manifests/status.pp
@@ -2,52 +2,41 @@
 # @api private
 class galera::status (
 ){
-  $available_when_donor = $galera::status_available_when_donor
-  $available_when_readonly = $galera::status_available_when_readonly
-  $port = $galera::status_port
-  $status_allow = $galera::status_allow
-  $status_host = $galera::status_host
-  $status_log_on_failure = $galera::status_log_on_failure
-  $status_log_on_success = $galera::status_log_on_success
-  $status_log_on_success_operator = $galera::status_log_on_success_operator
-  $status_password = $galera::status_password
-  $status_user = $galera::status_user
-
-  if ! $status_password {
+  if ! $galera::status_password {
     fail('galera::status_password unset. Please specify a password for the clustercheck MySQL user.')
   }
 
   if $galera::create_status_user {
-    if $status_allow != 'localhost' {
-      mysql_user { "${status_user}@${status_allow}":
+    if $galera::status_allow != 'localhost' {
+      mysql_user { "${galera::status_user}@${galera::status_allow}":
         ensure        => 'present',
-        password_hash => mysql_password($status_password),
+        password_hash => mysql_password($galera::status_password),
         require       => [Service['mysqld']]
       }
-      -> mysql_grant { "${status_user}@${status_allow}/*.*":
+      -> mysql_grant { "${galera::status_user}@${galera::status_allow}/*.*":
         ensure     => 'present',
         options    => [ 'GRANT' ],
         privileges => [ 'USAGE' ],
         table      => '*.*',
-        user       => "${status_user}@${status_allow}",
+        user       => "${galera::status_user}@${galera::status_allow}",
         before     => Anchor['mysql::server::end']
       }
       if $galera::create_root_my_cnf {
-        File["${::root_home}/.my.cnf"] -> Mysql_user["${status_user}@${status_allow}"]
+        File["${::root_home}/.my.cnf"] -> Mysql_user["${galera::status_user}@${galera::status_allow}"]
       }
     }
 
-    mysql_user { "${status_user}@localhost":
+    mysql_user { "${galera::status_user}@localhost":
       ensure        => 'present',
-      password_hash => mysql_password($status_password),
+      password_hash => mysql_password($galera::status_password),
       require       => [Service['mysqld']]
     }
-    -> mysql_grant { "${status_user}@localhost/*.*":
+    -> mysql_grant { "${galera::status_user}@localhost/*.*":
       ensure     => 'present',
       options    => [ 'GRANT' ],
       privileges => [ 'USAGE' ],
       table      => '*.*',
-      user       => "${status_user}@localhost",
+      user       => "${galera::status_user}@localhost",
       before     => Anchor['mysql::server::end']
     }
   }
@@ -75,13 +64,13 @@ class galera::status (
 
   xinetd::service { 'mysqlchk':
     server                  => '/usr/local/bin/clustercheck',
-    port                    => $port,
+    port                    => $galera::status_port,
     user                    => 'clustercheck',
     flags                   => 'REUSE',
     service_type            => 'UNLISTED',
-    log_on_success          => $status_log_on_success,
-    log_on_success_operator => $status_log_on_success_operator,
-    log_on_failure          => $status_log_on_failure,
+    log_on_success          => $galera::status_log_on_success,
+    log_on_success_operator => $galera::status_log_on_success_operator,
+    log_on_failure          => $galera::status_log_on_failure,
     require                 => [
       File['/usr/local/bin/clustercheck'],
       User['clustercheck'],
