3575464dbc6873fa0d03c03370e75d877f6cadcb
Benjamin Rechsteiner <38490328+aba-rechsteiner@users.noreply.github.com>
2020-08-17 16:09:32 +0200

    take ip-list without port (#162)
    * take ip list without port

--- manifests/init.pp
+++ manifests/init.pp
@@ -583,7 +583,7 @@ class galera(
     if ($::fqdn == $galera_master) {
       # If there are no other servers up and we are the master, the cluster
       # needs to be bootstrapped. This happens before the service is managed
-      $server_list = join($_nodes_tmp, ' ')
+      $server_list = join($galera_servers, ' ')
 
       exec { 'bootstrap_galera_cluster':
         command  => $params['bootstrap_command'],
