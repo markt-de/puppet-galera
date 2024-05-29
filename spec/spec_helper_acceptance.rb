fed6063f45bc442c086ffbe704c4da1d4dd66b6a
Lightning <lightning@lightning-online.de>
2024-05-29 14:24:17 +0200

    update spec/spec_helper_acceptance.rb
    fix order of requires/includes/configures to properly provide facts for
    spec_helper_acceptance_local.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,8 +2,8 @@
 
 require 'serverspec'
 require 'puppet_litmus'
-require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
 include PuppetLitmus
+PuppetLitmus.configure!
 
 if ENV['TARGET_HOST'].nil? || ENV['TARGET_HOST'] == 'localhost'
   puts 'Running tests against this machine !'
@@ -50,6 +50,7 @@ else
                                 else
                                   Net::SSH::Verifiers::Null.new
                                 end
+    # rubocop:enable Metrics/BlockNesting
     host = if ENV['TARGET_HOST'].include?(':')
              ENV['TARGET_HOST'].split(':').first
            else
@@ -78,3 +79,5 @@ else
     Specinfra.configuration.winrm = winrm
   end
 end
+
+require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
